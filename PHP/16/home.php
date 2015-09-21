6ff29e7cd082c9935fca111f55b4347b133185c1
Francesco Abeni <f.abeni@gibilogic.com>
2015-09-21 10:44:01 +0200

    Layout and functions files - fixes

---
+++ PHP/16/home.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title><?= TITLE ?></title>
+    </head>
+    <body>
+        <h1><?= $message ?></h1>
+        <p>Il numero degli studenti è <?= $numberOfStudents ?></p>
+        <h2>Elenco degli studenti</h2>
+        <ul>
+            <?php foreach ($students as $name => $role): ?>
+                <li><strong><?= $name ?></strong> (<?= $role ?>)</li>
+            <?php endforeach ?>
+        </ul>
+        <p>Il costo totale del caffè è <?= $coffeeCost ?></p>
+    </body>
+</html>
